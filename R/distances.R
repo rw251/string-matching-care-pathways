d10c2f7606b9bacdc97aeb31d42873f1880685bd
Richard <richard.williams2@man.ac.uk>
2015-05-15 15:23:28 +0100

    Some extra distance function. Code to load patients and pathways

---
+++ R/distances.R
@@ -0,0 +1,20 @@
+library(stringdist)
+
+normalisedLevenshteinDistance <- function(str1, str2){
+  return(stringdist(str1, str2, method="lv") / (nchar(str1) + nchar(str2)))
+}
+
+metricLevenshteinDistance <- function(str1, str2){
+  lv <- stringdist(str1, str2, method="lv");
+  return(2 * lv / (nchar(str1) + nchar(str2) + lv))
+}
+
+mystringdist <- function(str1, str2, method="osa") {
+  if (method == "nlv") {
+    return(normalisedLevenshteinDistance(str1, str2))
+  } else if (method == "mlv") {
+    return(metricLevenshteinDistance(str1, str2))
+  } else {
+    return(stringdist(str1, str2, method))
+  }
+}
--- analysis.R
+++ analysis.R
@@ -2,4 +2,19 @@
 #library(another_package)
 #source("R/functions.R")
 #source("R/utilities.R")
-library(stringdist)
+source("R/distances.R")
+library(igraph)
+
+mystringdist("cat","batter",method="lv")
+mystringdist("cat","batter",method="nlv")
+mystringdist("cat","batter",method="mlv")
+
+#load patient data
+
+patient.data <- read.table("data/sample-patient-file.txt", sep="\t", header=TRUE)
+
+#pathway <- read.graph("data/sample-pathway.graphml", format="graphml")
+pathway <- read.graph("data/sample-pathway.ncol", format="ncol", directed=TRUE)
+
+#plot(pathway)
+
