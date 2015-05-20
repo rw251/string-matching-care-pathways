1926ae378e7ab1868fc3bf63a57309b4164c64b3
Richard <richard.williams2@man.ac.uk>
2015-05-20 17:21:18 +0100

    Version working and tested against previously validated results.

--- R/pathway.R
+++ R/pathway.R
@@ -3,6 +3,17 @@ library(hash)
 pathway.events <- hash()
 pathway.uniCtr <- 97
 
+pathway.events[["stroke"]] <- "G"
+pathway.events[["first seen"]] <-"D"
+pathway.events[["arrive hospital"]] <-"H"
+pathway.events[["arrive stroke bed"]] <-"F"
+pathway.events[["discharge stroke unit"]] <-"I"
+pathway.events[["discharge hospital"]] <-"B"
+pathway.events[["brain imaging"]] <-"E"
+pathway.events[["thrombolysis"]] <-"A"
+pathway.events[["follow up scan"]] <-"C"
+
+
 pathway.event <- function( event ) {
   if(is.null(pathway.events[[ event ]])){
     pathway.events[[event]] <- intToUtf8(pathway.uniCtr)
@@ -16,17 +27,22 @@ pathway.eventList <- function( events ) {
 }
 
 pathway.load <- function(file, format="ncol") {
-  pathway.pathway <<- read.graph(file, format=format, directed=TRUE)
+  if (format=="ncol") {
+    pathway.pathway <<- read.graph(file, format=format, directed=TRUE)
+  } else {
+    pathway.pathway <<- read.graph(file, format=format) 
+  }
   return(pathway.pathway)
 }
 
 pathway.startVertices <- function () {
   d<-degree(pathway.pathway, mode="in") #get in degree of all vertices
-  return(as.list(names(d[d==0]))) #filter to those with in degree of 0 and get names
+  return(as.list(rep(1:length(d))[d==0])) #filter to those with in degree of 0 and get vertex indices
 }
 
 pathway.allPaths <- function() {
   sv <- pathway.startVertices()
+  output <- character()
   
   f.adjlist <- get.adjlist(pathway.pathway, mode = "out")
   
@@ -37,11 +53,12 @@ pathway.allPaths <- function() {
     v <- l[[length(l)]]  # Get the last vertex of the path
     
     if (length(f.adjlist[[v]]) == 0) {      
-      cat(paste(pathway.eventList(l)), "\n")  # output it (or save it in a list to be returned, if you prefer)
+      output <- c(output, paste(pathway.eventList(get.vertex.attribute(pathway.pathway, "name", l)), collapse=""))  # output it (or save it in a list to be returned, if you prefer)
     }
     
     for (i in f.adjlist[[v]]) {
-      sv[[ length(sv)+1 ]] <- c(l, names(pathway.pathway[[i]]))
+      sv[[ length(sv)+1 ]] <- c(l, i)
     }
   } 
+  return(output)
 }
--- analysis.R
+++ analysis.R
@@ -7,19 +7,79 @@ source("R/patient.R")
 ####################
 ## Set parameters ##
 ####################
-config.patient.filename <- "data/sample-patient-file.txt"
+#config.patient.filename <- "data/sample-patient-file.txt"
+config.patient.filename <- "data/fake-sinap-patient-file.txt"
 config.patient.date.format <- "%Y/%m/%d %H:%M"  # see help(strptime)
 
-config.pathway.filename <- "data/sample-pathway.ncol"
-config.pathway.format <- "ncol"  #Also "edgelist", "pajek", "ncol", "lgl", "graphml", "dimacs", "graphdb", "gml", "dl" see http://igraph.org/r/doc/read.graph.html
+#config.pathway.filename <- "data/sample-pathway.ncol"
+config.pathway.filename <- "data/sinap-pathway.gml"
+config.pathway.format <- "gml"  #Also "edgelist", "pajek", "ncol", "lgl", "graphml", "dimacs", "graphdb", "gml", "dl" see http://igraph.org/r/doc/read.graph.html
 
 #load patient data and get individual pathway strings
-patient.pathways(config.patient.filename, dateFormat=config.patient.date.format)
+patients <- patient.pathways(config.patient.filename, dateFormat=config.patient.date.format)
 
 #load pathway
 pathway <- pathway.load(config.pathway.filename, format=config.pathway.format)
 plot(pathway)
 
-pathway.allPaths()
+all.paths <- pathway.allPaths()
 
+for (i in 1:length(patients)) {
+  min.dist <- 100000
+  min.paths <- character()
+  
+  for (j in 1:length(all.paths)) {
+    dist <- distance.stringdist(patients[[keys(patients)[[i]]]], all.paths[[j]], method="lcs2")
+    #tested with lv, nlv, mlv, dm, ndm, lcs1, lcs2 - all exact matches to SINAP results
+    if (dist < min.dist) {
+      min.dist <- dist
+      min.paths <- c(all.paths[[j]])
+    } else if (dist == min.dist) {
+      min.paths <- c(min.paths, all.paths[[j]])
+    }    
+  }
+  if (length(min.paths) > 1) {
+    patients[[keys(patients)[[i]]]] <- c(patients[[keys(patients)[[i]]]], paste(length(min.paths),"matches"), min.dist)
+  } else {
+    patients[[keys(patients)[[i]]]] <- c(patients[[keys(patients)[[i]]]], min.paths[[1]], min.dist)
+  }
+}
 
+patients
+
+## missing functions
+# SimulatePatients
+# GeneratePatientPathwayData - both for simulating patients in pathway
+# Enumerate cycles - thought didn't use this for SINAP..?
+# AttemptOrdering(midnightEvents, sameDayEvents) - for events at midnight (i.e. where time lost) - can we slot them in
+#
+#private static bool AttemptOrdering(IEnumerable<Event> midnightEvents, List<Event> sameDayEvents)
+#{
+#  
+#  foreach (var midnightEvent in midnightEvents)
+#{
+#    var i = GetLocation(sameDayEvents, midnightEvent);
+#    if(i<0) return false;
+#    sameDayEvents.Insert(i, midnightEvent);
+#  }
+#
+#return true;
+#}
+#
+#private static int GetLocation(List<Event> events, Event eventToFit)
+#{
+#  var done = false;
+#  var rtn = -1;
+#  for(var i = 0; i < events.Count; i++)
+#  {
+#    var c = CompareEvents(events[i], eventToFit);
+#    if (c == EventComparison.Both || c == EventComparison.Neither) return -1;
+#    if (!done && c == EventComparison.GreaterThan)
+#    {
+#      done = true;
+#      rtn = i;
+#    }
+#    else if (done && c == EventComparison.LessThan) return -1;
+#  }
+#  return rtn < 0 ? events.Count : rtn;
+#}
