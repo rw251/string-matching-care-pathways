2713d8317ac1d777c0157c6c2206d8ceaf580328
Richard <richard.williams2@man.ac.uk>
2015-05-20 10:42:57 +0100

    Functions refactored out of analysis.R into the patient and pathway helper scripts.

--- analysis.R
+++ analysis.R
@@ -1,21 +1,42 @@
-#library(some_package)
-#library(another_package)
-#source("R/functions.R")
-#source("R/utilities.R")
-source("R/distances.R")
 library(igraph)
+library(hash)
+source("R/distance.R")
+source("R/pathway.R")
+source("R/patient.R")
 
-mystringdist("cat","batter",method="lv")
-mystringdist("cat","batter",method="nlv")
-mystringdist("cat","batter",method="mlv")
+####################
+## Set parameters ##
+####################
+config.patient.filename <- "data/sample-patient-file.txt"
+config.patient.date.format <- "%Y/%m/%d %H:%M"  # see help(strptime)
 
-#load patient data
+config.pathway.filename <- "data/sample-pathway.ncol"
+config.pathway.format <- "ncol"  #Also "edgelist", "pajek", "ncol", "lgl", "graphml", "dimacs", "graphdb", "gml", "dl" see http://igraph.org/r/doc/read.graph.html
 
-patient.data <- read.table("data/sample-patient-file.txt", sep="\t", header=TRUE)
+#load patient data and get individual pathway strings
+patient.pathways(config.patient.filename, dateFormat=config.patient.date.format)
 
-#pathway <- read.graph("data/sample-pathway.graphml", format="graphml")
-pathway <- read.graph("data/sample-pathway.ncol", format="ncol", directed=TRUE)
+#load pathway
+pathway <- pathway.load(config.pathway.filename, format=config.pathway.format)
 
+sv <- pathway.startVertices()
+
+for(i in 1:length(sv)){
+  
+}
 #plot(pathway)
 
+#Get all paths
+# assume no recursion
 
+#select start node
+
+#flag as visited
+
+#visit each
+
+#identify start nodes
+
+# add two nodes
+# add 1 to indegree of second
+#loop through nodes and return those with indeg of 0
